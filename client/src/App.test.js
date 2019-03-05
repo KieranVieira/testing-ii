20094960eee6e9ff7a3d9ae42fd94cc1115fa923
KieranVieira <kieranvieira@live.com>
2019-03-05 15:54:52 -0500

    Added tests for renders on components

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { render } from 'react-testing-library';
 
 import App from './App';
+import Display from './Display/Display';
+import Dashboard from './Dashboard/Dashboard';
 
 describe('<App/>', () => {
   it('renders without crashing', () => {
     render(<App />);
   });
 });
-describe('<Dashboard/>', () => {
-  it('renders without crashing', () => {
-    render(<App />);
-  });
-});
+
 describe('<Display/>', () => {
   it('renders without crashing', () => {
-    render(<App />);
+    render(<Display />);
+  });
+});
+
+describe('<Dashboard/>', () => {
+  it('renders without crashing', () => {
+    render(<Dashboard />);
   });
 });
