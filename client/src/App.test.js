20c8054b3aff98a4cfc0115b322b8426edcd936b
KieranVieira <kieranvieira@live.com>
2019-03-05 19:13:42 -0500

    Added click event testing for balls and strikes

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 
 import App from './App';
 import Dashboard from './Dashboard/Dashboard';
@@ -7,5 +7,25 @@ import Dashboard from './Dashboard/Dashboard';
 describe('<App/>', () => {
   it('renders without crashing', () => {
     render(<App />);
+  });
+
+  it('Should update strikes when clicked', () => {
+    const { getByTestId } = render(<App />)
+
+    const strikeButton = getByTestId(/strikeBtn/i);
+
+    fireEvent.click(strikeButton)
+
+    expect(getByTestId(/strikes-count/i).textContent).toBe("Strikes: 1")
+  });
+
+  it('Should update balls when clicked', () => {
+    const { getByTestId } = render(<App />)
+
+    const strikeButton = getByTestId(/ballBtn/i);
+
+    fireEvent.click(strikeButton)
+
+    expect(getByTestId(/balls-count/i).textContent).toBe("Balls: 1")
   });
 });
--- client/src/Display/Display.js
+++ client/src/Display/Display.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Display = props => {
   return (
     <div>
-      <h3 data-testid="balls">Balls: {props.balls}</h3>
-      <h3 data-testid="strikes">Strikes: {props.strikes}</h3>
+      <h3 data-testid="balls-count">Balls: {props.balls}</h3>
+      <h3 data-testid="strikes-count">Strikes: {props.strikes}</h3>
     </div>
   )
 }
